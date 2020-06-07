8bb0c494e9af8d692795749775dc76ce43a9366f
JiwooKim-dev <jiwoo.kim.dev@gmail.com>
2020-06-08 04:14:41 +0900

    Update Face Alignment test (폴더 단위 테스트)

--- FaceAlignmentTest.py
+++ FaceAlignmentTest.py
@@ -2,6 +2,8 @@ import sys
 import dlib
 import cv2
 import openface
+import glob
+import os
 
 predictor_model = "shape_predictor_68_face_landmarks.dat"
 
@@ -9,20 +11,23 @@ face_detector = dlib.get_frontal_face_detector()
 face_pose_predictor = dlib.shape_predictor(predictor_model)
 face_aligner = openface.AlignDlib(predictor_model)
 
-file_name = sys.argv[1]
+faces_folder_path = sys.argv[1]
 
-image = cv2.imread(file_name)
+for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
+    print("Processing file: {}".format(f))
 
-detected_faces = face_detector(image, 1)
+    image = cv2.imread(f, cv2.IMREAD_COLOR)
 
-print("Found {} faces in the image file {}".format(len(detected_faces), file_name))
+    detected_faces = face_detector(image, 1)
+    print("Number of faces detected: {}".format(len(detected_faces)))
 
-for i, face_rect in enumerate(detected_faces):
-    print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(),
-                                                                             face_rect.right(), face_rect.bottom()))
+    for i, face_rect in enumerate(detected_faces):
+        print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(),
+                                                                                 face_rect.right(), face_rect.bottom()))
 
-    pose_landmarks = face_pose_predictor(image, face_rect)
+        pose_landmarks = face_pose_predictor(image, face_rect)
 
-    alignedFace = face_aligner.align(534, image, face_rect, landmarkIndices=openface.AlignDlib.INNER_EYES_AND_BOTTOM_LIP)
+        alignedFace = face_aligner.align(534, image, face_rect,
+                                         landmarkIndices=openface.AlignDlib.INNER_EYES_AND_BOTTOM_LIP)
 
-    cv2.imwrite("aligned_face_{}.jpg".format(i), alignedFace)
+        cv2.imwrite(f + "{}.jpg".format(i), alignedFace)
